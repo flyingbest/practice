bc6e504c6e104396649a71398b87702f8b01f69b
flyingbest <taeyoon4715@gmail.com>
2015-11-23 22:59:15 +0900

    w

--- Git_prac/test.cpp
+++ Git_prac/test.cpp
@@ -2,6 +2,20 @@
 #include <string>
 using namespace std;
 
+typedef struct node{
+	int data;
+	struct node* next;
+} Node;
+
+void printlist(Node* head){
+	Node* cur = head;
+	while(cur){
+		cout << cur-data << " ";
+		cur = cur->next;
+	}
+	cout << endl;
+}
+
 int main(){
 	cout << " test " << endl;
 	return 0;
